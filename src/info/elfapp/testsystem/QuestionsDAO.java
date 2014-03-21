d5289f37b038b28110d6ee9255639cf78b75eb05
Andrey <grizzly-terror@yandex.ru>
2014-03-21 20:04:22 +0400

    commit from ide

--- src/info/elfapp/testsystem/QuestionsDAO.java
+++ src/info/elfapp/testsystem/QuestionsDAO.java
@@ -9,9 +9,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- *some text
- **/
 
 public class QuestionsDAO {
     private DataSource dataSource;
