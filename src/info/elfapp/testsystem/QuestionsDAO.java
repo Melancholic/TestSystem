e2a4a65907a1dfa6ae406422a6da122e174e9dd2
Andrey <grizzly-terror@yandex.ru>
2014-03-21 19:56:27 +0400

    commit from ide

--- src/info/elfapp/testsystem/QuestionsDAO.java
+++ src/info/elfapp/testsystem/QuestionsDAO.java
@@ -1,50 +1,49 @@
 package info.elfapp.testsystem;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
 
 public class QuestionsDAO {
-	
-	private DataSource dataSource;
 
-	public QuestionsDAO(){
+    private DataSource dataSource;
+
+    public QuestionsDAO() {
         try {
-            Context initContext  = new InitialContext();
-            Context envContext  = (Context)initContext.lookup("java:/comp/env");
-            dataSource = (DataSource)envContext.lookup("jdbc/tsystemdb"); 
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            dataSource = (DataSource) envContext.lookup("jdbc/tsystemdb");
         } catch (NamingException e) {
             e.printStackTrace();
         }
-	}
-	
-	public String getUserQuestion(int questionID){
-		String r = null;
+    }
+
+    public String getUserQuestion(int questionID) {
+        String r = null;
 
         try {
-            String  QUESTION = "Question";
-			Connection connection = dataSource.getConnection();
-	        String query = "SELECT "+QUESTION +" FROM Questions WHERE ID = ?";
-	        PreparedStatement prepStmt = connection.prepareStatement(query);
-	        prepStmt.setInt(1, questionID);
-	        ResultSet resultSet = prepStmt.executeQuery();
+            String QUESTION = "Question";
+            Connection connection = dataSource.getConnection();
+            String query = "SELECT " + QUESTION + " FROM Questions WHERE ID = ?";
+            PreparedStatement prepStmt = connection.prepareStatement(query);
+            prepStmt.setInt(1, questionID);
+            ResultSet resultSet = prepStmt.executeQuery();
             if (resultSet.next()) {
-            	r = new String();
-            	r = resultSet.getString(QUESTION);
+                r = new String();
+                r = resultSet.getString(QUESTION);
             }
             connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-		return r;
-	}
+        return r;
+    }
 
 }
